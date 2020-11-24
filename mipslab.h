16971f7baca67c7a3404d265bd05cc44022f5002
Tim Olsén <>
2020-11-24 17:29:42 +0100

    La till siffror

--- mipslab.h
+++ mipslab.h
@@ -41,6 +41,7 @@ extern const uint8_t const font[128*8];
 extern char textbuffer[4][16];
 
 extern uint8_t field[16*128];
+extern const uint8_t numbers[10*3];
 
 /* Declare functions written by students.
    Note: Since we declare these functions here,
